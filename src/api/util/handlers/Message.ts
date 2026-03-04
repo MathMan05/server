ac11d45e87a82e46eedc5bad7353ae8ac2a16f0a
MathMan05 <mathmanrm@gmail.com>
2026-03-04 12:06:38 -0600

    remove log

--- src/api/util/handlers/Message.ts
+++ src/api/util/handlers/Message.ts
@@ -782,7 +782,6 @@ export async function postHandleMessage(message: Message) {
     const linkMatches = content?.match(LINK_REGEX) || [];
     message.clean_data();
     const data = { ...message.toJSON() };
-    console.log(message);
 
     const currentNormalizedUrls = new Set<string>();
     for (const link of linkMatches) {
